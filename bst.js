c2ed2c6a2934cc8d2e7ba381527eef7141823ab7
Owcollier <owcollier@gmail.com>
2018-01-30 13:59:35 -0500

    added main function to test BinarySearchTree class

--- bst.js
+++ bst.js
@@ -112,3 +112,18 @@ class BinarySearchTree {
     return this.left._findMin();
   }
 }
+
+function main() {
+  const BST = new BinarySearchTree();
+  BST.insert(3);
+  BST.insert(1);
+  BST.insert(4);
+  BST.insert(6);
+  BST.insert(9);
+  BST.insert(2);
+  BST.insert(5);
+  BST.insert(7);
+  console.dir(BST, {depth: null, colors:true});
+}
+
+main();
